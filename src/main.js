895466a8157eed12e61315f3ee4de7470e70b492
qwxying <qwxying@gmail.com>
2020-05-01 21:07:59 +0800

    适配了PC页面

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const $siteList = $(".siteList");
 const $lastLi = $siteList.find("li.last");
 const x = localStorage.getItem("x");
 const xObject = JSON.parse(x);
+
 const hashMap = xObject || [
   {
     logo: "A",
@@ -16,39 +17,74 @@ const hashMap = xObject || [
     url: "https://tucao.one",
   },
 ];
+
+const addNewLink = () => {
+  $("#addButton").on("click", () => {
+    let url = window.prompt("请输入要添加的网址：");
+    if (url === "" || null) {
+      alert("网址不能为空！");
+    } else if (url.indexOf("http") !== 0) {
+      url = "https://" + url;
+      hashMap.push({
+        logo: simplifyUrl(url)[0],
+        url: url,
+      });
+      render();
+    }
+  });
+};
+
+const simplifyUrl = (url) => {
+  return url
+    .toLowerCase()
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(".com", "")
+    .replace(".cn", "")
+    .replace(".one", "")
+    .replace(".io", "")
+    .replace(/\/.*/, ""); //删除以/开头的内容
+};
+
 const render = () => {
   $siteList.find("li:not(.last)").remove();
-  hashMap.forEach((node) => {
+  hashMap.forEach((node, index) => {
     //   node.logo = node.url.substring(8, 11).toLowerCase() === "www" ? url[12] : url[8];
     const $li = $(`
           <li>
-              <a href="${node.url}">
               <div class="site">
-                  <div class="logo">${node.logo.toUpperCase()}</div>
-                  <div class="link">${node.url.slice(8).toLowerCase()}</div>
+                  <div class="logo">${node.logo}</div>
+                  <div class="link">${simplifyUrl(node.url)}</div>
+                  <div class="close">x</div>
               </div>
-              </a>
           </li>
         `).insertBefore($lastLi);
+    $li.on("click", () => {
+      window.open(node.url, "_self");
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation(); //阻止冒泡
+      hashMap.splice(index, 1);
+      render();
+    });
   });
 };
 
 render();
 
-$("#addButton").on("click", () => {
-  let url = window.prompt("请输入要添加的网址：");
-  if (url.indexOf("http") !== 0) {
-    url = "https://" + url;
-  }
-
-  hashMap.push({
-    logo: url[0],
-    url: url,
-  });
-  render();
-});
+addNewLink();
 
 window.onbeforeunload = () => {
   const string = JSON.stringify(hashMap);
   localStorage.setItem("x", string);
 };
+
+$(document).on("keypress", (e) => {
+  const { key } = e;
+  for (let i = 0; i < hashMap.length; i++) {
+    if (hashMap[i].logo.toLowerCase() === key) {
+      window.open(hashMap[i].url, "_self");
+    }
+  }
+});
