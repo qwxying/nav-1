8e153fd8796e3f272dfc1289a69458b2f132475f
qwxying <qwxying@gmail.com>
2020-05-01 17:41:56 +0800

    实现localStorage

--- src/main.js
+++ src/main.js
@@ -1,19 +1,54 @@
+const $siteList = $(".siteList");
+const $lastLi = $siteList.find("li.last");
+const x = localStorage.getItem("x");
+const xObject = JSON.parse(x);
+const hashMap = xObject || [
+  {
+    logo: "A",
+    url: "https://www.acfun.cn",
+  },
+  {
+    logo: "B",
+    url: "https://www.bilibili.com",
+  },
+  {
+    logo: "C",
+    url: "https://tucao.one",
+  },
+];
+const render = () => {
+  $siteList.find("li:not(.last)").remove();
+  hashMap.forEach((node) => {
+    //   node.logo = node.url.substring(8, 11).toLowerCase() === "www" ? url[12] : url[8];
+    const $li = $(`
+          <li>
+              <a href="${node.url}">
+              <div class="site">
+                  <div class="logo">${node.logo.toUpperCase()}</div>
+                  <div class="link">${node.url.slice(8).toLowerCase()}</div>
+              </div>
+              </a>
+          </li>
+        `).insertBefore($lastLi);
+  });
+};
+
+render();
+
 $("#addButton").on("click", () => {
   let url = window.prompt("请输入要添加的网址：");
   if (url.indexOf("http") !== 0) {
     url = "https://" + url;
   }
-  let newLogo = url.substring(8, 11).toLowerCase() === "www" ? url[12] : url[8];
 
-  const $siteList = $(".siteList");
-  const $li = $(`
-    <li>
-        <a href="${url}">
-        <div class="site">
-            <div class="logo">${newLogo.toUpperCase()}</div>
-            <div class="link">${url.slice(8).toLowerCase()}</div>
-        </div>
-        </a>
-    </li>
-  `).insertBefore($siteList.find("li.last"));
+  hashMap.push({
+    logo: url[0],
+    url: url,
+  });
+  render();
 });
+
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap);
+  localStorage.setItem("x", string);
+};
